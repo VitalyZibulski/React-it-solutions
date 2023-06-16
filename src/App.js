35a610150fa5b9a132ebeca785baf3a692590445
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-16 12:12:27 +0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, {createGlobalStyle} from "styled-components";
+import Navbar from "./components/navbar/Navbar";
+import React from "react";
+
+const GlobalStyles = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-color: white;
+  @import url("https://fonts.googleapis.com/css2?family=Splash&display=swap");
+  *{
+    font-family: 'Roboto', sans-serif;
+  }
+}
+`;
+
+const Container = styled.div`
+  background-color: white;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Container>
+        <Navbar />
+      </Container>
+    </>
   );
 }
 
